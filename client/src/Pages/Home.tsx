0c6449292ee74f7fa0c0cba29a155982e8194841
Rikky <rozsarichardzon@gmail.com>
2025-11-06 12:44:14 +0100

    React alap rendszer

---
+++ client/src/Pages/Home.tsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+
+function Home() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Hello
+      </div>
+    </>
+  )
+}
+
+export default Home
